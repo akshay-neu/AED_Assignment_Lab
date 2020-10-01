7370ab4e36c4f381b418c79b9a33ccd8b352b7e0
akshay-neu <khandelwal.ak@northeastern.edu>
2020-09-30 23:08:11 -0400

    Assignment 1

---
+++ Assignment-1/src/Business/Person.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Akshay
+ */
+public class Person {
+    private String name;
+    private String address;
+    private String dob;
+    private int telephone = 0;
+    private String faxNo;
+    private String emailId;
+    private String ssn;
+    private String medicalRecordNo;
+    private String healthPlanNo;
+    private long bankAccNo = 0;
+    private String certificateNo;
+    private String vehicleIdentifier; 
+    private String deviceIdentifier;
+    private String linkedIn;
+    private String internetProtocolAdd;
+    private String biometricIdentifier;
+    private String imagePath;
+    private String uniqueIndentifier;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(String faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecordNo() {
+        return medicalRecordNo;
+    }
+
+    public void setMedicalRecordNo(String medicalRecordNo) {
+        this.medicalRecordNo = medicalRecordNo;
+    }
+
+    public String getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(String healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public long getBankAccNo() {
+        return bankAccNo;
+    }
+
+    public void setBankAccNo(long bankAccNo) {
+        this.bankAccNo = bankAccNo;
+    }
+
+    public String getCertificateNo() {
+        return certificateNo;
+    }
+
+    public void setCertificateNo(String certificateNo) {
+        this.certificateNo = certificateNo;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocolAdd() {
+        return internetProtocolAdd;
+    }
+
+    public void setInternetProtocolAdd(String internetProtocolAdd) {
+        this.internetProtocolAdd = internetProtocolAdd;
+    }
+
+    public String getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(String biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getUniqueIndentifier() {
+        return uniqueIndentifier;
+    }
+
+    public void setUniqueIndentifier(String uniqueIndentifier) {
+        this.uniqueIndentifier = uniqueIndentifier;
+    }
+    
+    
+    
+    
+}
